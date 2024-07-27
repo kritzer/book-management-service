e0cfaf306cede795f036904dff27fb5543a64ac9
Kritsada Patchima <>
2024-07-27 13:15:08 +0700

    - change class parameter for update and insert book - add check id for delete book - create method for validate upsert book request

--- src/main/java/com/example/demo/service/BookService.java
+++ src/main/java/com/example/demo/service/BookService.java
@@ -1,12 +1,18 @@
 package com.example.demo.service;
 
+import com.example.demo.exception.CentralizedError;
+import com.example.demo.exception.CentralizedException;
 import com.example.demo.model.entity.Book;
+import com.example.demo.model.payload.request.UpsertBookRequest;
 import com.example.demo.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+
+import static com.example.demo.types.ErrorResponseEnum.UPDATE_BOOK_BAD_REQUEST;
 
 @Service
 public class BookService {
@@ -22,22 +28,37 @@ public class BookService {
         return bookRepository.findById(id);
     }
 
-    public Book addBook(Book book) {
+    public Book addBook(UpsertBookRequest request) {
+        Book book = new Book();
+        book.setTitle(request.getTitle());
+        book.setAuthor(request.getAuthor());
+        book.setIsbn(request.getIsbn());
+        book.setPublishedDate(request.getPublishedDate());
         return bookRepository.save(book);
     }
 
-    public Book updateBook(Long id, Book bookDetails) {
+    public Book updateBook(Long id, UpsertBookRequest request) {
         Book book = bookRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Book not found"));
-        book.setTitle(bookDetails.getTitle());
-        book.setAuthor(bookDetails.getAuthor());
-        book.setIsbn(bookDetails.getIsbn());
-        book.setPublishedDate(bookDetails.getPublishedDate());
+        book.setTitle(request.getTitle());
+        book.setAuthor(request.getAuthor());
+        book.setIsbn(request.getIsbn());
+        book.setPublishedDate(request.getPublishedDate());
         return bookRepository.save(book);
     }
 
     public void deleteBook(Long id) {
+        bookRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Book not found"));
         bookRepository.deleteById(id);
+    }
+
+    public void validateBookRequest(UpsertBookRequest request) throws CentralizedException {
+        List<String> errorStrList = request.validate();
+        if (!errorStrList.isEmpty()) {
+            var errorResponse = new CentralizedError(UPDATE_BOOK_BAD_REQUEST.getCode(), errorStrList.toString());
+            throw new CentralizedException(errorResponse, HttpStatus.NOT_FOUND);
+        }
     }
 
 }
