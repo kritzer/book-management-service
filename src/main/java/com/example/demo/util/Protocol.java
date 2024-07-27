f68ccde70e7601dd17dfd3af4e45f2a416ac2c80
Kritsada Patchima <>
2024-07-27 13:13:21 +0700

    create variable for book api

--- src/main/java/com/example/demo/util/Protocol.java
+++ src/main/java/com/example/demo/util/Protocol.java
@@ -3,4 +3,6 @@ package com.example.demo.util;
 public class Protocol {
     public static final String API = "/api";
     public static final String API_VERSION_1 = API + "/v1";
+    public static final String BOOKS_API = API_VERSION_1 + "/books";
+    public static final String ID_PATH_VARIABLE = "/{id}";
 }
