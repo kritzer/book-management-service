2dd264ddbb06fcca9bafed645a2d0b07d90213e5
Kritsada Patchima <>
2024-07-27 11:59:50 +0700

    add sql script file for example datasource

---
+++ src/main/resources/defaultData/data.sql
@@ -0,0 +1,3 @@
+insert into BOOK (PUBLISHED_DATE, AUTHOR, ISBN, TITLE) values (CURRENT_TIMESTAMP(), 'test Author', 'Test isbn', 'test title');
+
+insert into BOOK (PUBLISHED_DATE, AUTHOR, ISBN, TITLE) values (CURRENT_TIMESTAMP(), 'test Author 2', 'Test isbn 2', 'test title 2');
