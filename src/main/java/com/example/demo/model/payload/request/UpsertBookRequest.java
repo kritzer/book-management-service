99ff3bb230e468ada8990120c903624de1a0a3e5
Kritsada Patchima <>
2024-07-27 13:16:44 +0700

    remove validate isbn

--- src/main/java/com/example/demo/model/payload/request/UpsertBookRequest.java
+++ src/main/java/com/example/demo/model/payload/request/UpsertBookRequest.java
@@ -100,10 +100,6 @@ public class UpsertBookRequest {
             errors.add("Author must not exceed 255 characters");
         }
 
-//        if (isbn == null || !isbn.matches("^(?:ISBN(?:-1[03])?:? )?(?=[0-9X]{10}$|(?=(?:[0-9]+[- ]){3})[- 0-9X]{13}$|97[89][0-9]{10}$|(?=(?:[0-9]+[- ]){4})[- 0-9]{17}$)(?:97[89][- ]?)?[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9X]$")) {
-//            errors.add("Invalid ISBN format");
-//        }
-
         if (publishedDate == null) {
             errors.add("Published date cannot be null");
         }
